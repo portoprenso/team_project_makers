ff76c671a21fcce3218bd06433cd5033c9cd9a62
portoprenso <portoprenso@gmail.com>
2021-04-29 21:46:20 +0600

    test

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import ProductList from '../Products/ProductList';
+import Catalog1 from '../Products/Catalog1'
 import MainCarousel from '../Carousel/Carousel';
 
 const HomePage = () => {
