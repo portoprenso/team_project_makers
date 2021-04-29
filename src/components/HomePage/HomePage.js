647bd0fd024357a8753e0610ce953d4892e59e27
portoprenso <portoprenso@gmail.com>
2021-04-29 18:37:11 +0600

    test commit to check conflicts

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import ProductList from '../Products/ProductList';
+import Catalog1 from '../Products/Catalog1'
 
 const HomePage = () => {
     return (
